d7f61dc19b778fd88ba6d49d8b5d1eee2d8ed237
Raikyo <thunderlord@list.ru>
2020-10-13 19:13:32 +0300

    попытки сделать предыдущую и новую

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,55 +1,67 @@
 export default class ColumnChart {
-element;//html element
-	constructor(set){
-		//инициализация переменных
-		if ('data' in set )this.data = set.data;
-		else this.data = [];
-		if ('label' in set)this.label = set.label;
-		else this.label = "";
-		if ('value' in set)this.value = set.value;
-		else this.value = 0;
-		if ('link' in set)this.link = set.link;
-		else this.link = "";
-		//вызов функций в конструкторе
-		this.render();
+	subElements = {};
+	chartHeight = 50;//уставка высоты барчартов
+	
+	constructor({
+		data = [],
+		label = '',
+		link = '',
+		value = 0
+	} = {}){
+			//инициализация переменных
+			this.data =	data;
+			this.label = label;
+			this.value = value;
+			this.link = link;
+			//вызов функций в конструкторе
+			this.render();
 	}
-	render(){
-		let skeleton = "";
-		//проверка на пустые данные
-		if (this.data.length>0 ) skeleton =`"background:;"`;
-		else skeleton = `"background: url('charts-skeleton.svg') center no-repeat;"`;	
-		//Функция что делает набор строк по массиву
-		function strSet(arr){
-		let str ="";
-		arr.forEach(function(item) {
-							str += `<div style="--value: ${item}/100*50" data-tooltip="${item/100*50}%"></div>`
-							}); 
-		return str;	
+	getColBody(data){
+		// задание коэффициэнта
+		const maxVal = Math.max(...data);
+		const scale = this.chartHeight/maxVal;
+		return data.map(item => {
+			//считаем процент
+			const percent = (item / maxVal * 100).toFixed(0); //и округляем
+			return `<div style="--value: ${Math.floor(item*scale)}" data-tooltip="${percent}"></div>`;
+		}).join("");//преобразуем в строку
+	}
+	getSubElement(element){
+		const elem =element.querySelectorAll("[data-element]");
+		return [...elem].reduce((acc,subElements) =>{
+			acc[subElements.dataset.element]=subElements;
+			return acc
+		},{});
 		}
-		//задание диф элемента
-		const div = document.createElement('div');
-			//наполняем данными
-			div.innerHTML = `
-				<div class="dashboard__chart_${this.label}">
-					<div class="column-chart" style=`+skeleton+`}>
-						<div class="column-chart__title">
-							Total ${this.label}
-							<a href="/${this.link}" class="column-chart__link">View all</a>
-						</div>
-						<div class="column-chart__container">
-							<div data-element="header" class="column-chart__header">${this.value}</div> 
-								<div data-element="body" class="column-chart__chart">
-							` +
-							strSet(this.data);
-							+
-							`
-							</div>
+		
+	render(){
+		//задание див элемента
+		const element = document.createElement('div');
+		element.innerHTML = ` 
+				<div class="column-chart column-chart_loading" style="...${this.chartHeight}">
+					<div class="column-chart__title">
+						Total ${this.label}
+						<a href="/${this.link}" class="column-chart__link">View all</a>
+					</div>
+					<div class="column-chart__container">
+						<div data-element="header" class="column-chart__header">${this.value}</div> 
+						<div data-element="body" class="column-chart__chart">
+							${this.getColBody(this.data)}
 						</div>
 					</div>
-				</div>				
+				</div>						
 			`;
-		this.element=div;
+		this.element = element.firstElementChild;
+		if (this.data.length){//вычищаем скелетон стоящий по умолчанию
+			this.element.classList.remove(`column-chart_loading`);
+		}
+		this.subElements = this.getSubElement(this.element);
 	}
+	//обновление при изменении входных данных
+	update(data){
+	this.subElements.body.innerHTML = this.getColBody(data);
+	}
+	//удаление элемента
 	remove(){
 		this.element.remove();
 	}
--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,42 @@
 export default class NotificationMessage {
+	constructor(notification,{
+		duration = 0,
+		type = ""
+	} = {}){
+			//инициализация переменных
+			this.note =	notification;
+			this.dur = duration;
+			this.type = type;
+			//вызов функций в конструкторе
+	}
+		
+	render(){
+		//задание див элемента
+		const element = document.createElement('div');
+		element.innerHTML = ` 
+				<div class="notification ${this.type}" style="--value:${this.dur}s">
+					<div class="timer"></div>
+					<div class="inner-wrapper">
+						<div class="notification-header">${this.type}</div>
+						<div class="notification-body">
+						${this.note}
+					</div>
+				</div>	
+			`;
+		this.element = element.firstElementChild;
+		document.body.append(element);
+		setTimeout(() => element.remove(), this.dur);
 
+	}
+	show(){
+		this.render();
+	}
+	//удаление элемента
+	remove(){
+		this.element.remove();
+	}
+	
+	destroy(){
+		this.remove();
+	}
 }
